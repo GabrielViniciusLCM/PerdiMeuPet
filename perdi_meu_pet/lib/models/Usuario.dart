490eb0159d6b693f645e87b5f8d50616edfcc691
Gabriel Vinicius <gabrielviniciuslcm@gmail.com>
2024-10-29 21:39:29 -0300

    feat: autenticacao com provider mockado

--- perdi_meu_pet/lib/models/Usuario.dart
+++ perdi_meu_pet/lib/models/Usuario.dart
@@ -4,10 +4,12 @@ class Usuario {
     required this.nome,
     required this.email,
     required this.telefone,
+    required this.senha,
   });
 
   final String id;
   final String nome;
   final String email;
   final String telefone;
+  final String senha; 
 }
