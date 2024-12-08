464faf91b1df83b2ace35c7dcfabfb28fd1213aa
ianco <ianco.so@gmail.com>
2024-12-07 23:19:03 -0300

    :sparkles: feat(provider): add pet provier

--- perdi_meu_pet/lib/main.dart
+++ perdi_meu_pet/lib/main.dart
@@ -5,16 +5,10 @@ import 'package:provider/provider.dart';
 
 import '/utils/app_routes.dart';
 import '../screens/home_screen.dart';
+import 'domain/provider/pet_provider.dart';
+import 'domain/provider/post_provider.dart';
 import 'domain/provider/user_provider.dart';
-// import './tabs/ProfileTab.dart';
-
 void main() {
-  // runApp(
-  //   ChangeNotifierProvider(
-  //     create: (context) => UsuarioProvider(),
-  //     child: FindMyPetApp(),
-  //   ),
-  // );
   runApp(const FindMyPetApp());
 }
 
@@ -25,18 +19,29 @@ class FindMyPetApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (context) => UserProvider()),
+        ChangeNotifierProvider(create: (_) => UserProvider()),
         ChangeNotifierProvider(create: (context) => CommentProvider()),
+                ChangeNotifierProvider(create: (_) => UserProvider()), 
+        ChangeNotifierProxyProvider<UserProvider, PetProvider>(
+          create: (_) => PetProvider(UserProvider()),
+          update: (_, userProvider, previousPetProvider) => PetProvider(userProvider),
+        ),
+        ChangeNotifierProxyProvider(
+          create: (_) => PostProvider(), 
+          update: (_, userProvider, previousPostProvider) => PostProvider(),
+        ),
       ],
       child: MaterialApp(
-          theme: ThemeData(
-            primarySwatch: Colors.teal,
-            primaryColor: Colors.teal,
-          ),
-          home: HomeScreen(),
-          routes: {
-            AppRoutes.PROFILE: (ctx) => ProfileScreen(),
-          }),
+        theme: ThemeData(
+          primarySwatch: Colors.teal,
+          primaryColor: Colors.teal,
+        ),
+        home: HomeScreen(),
+        routes: {
+          AppRoutes.PROFILE: (ctx) => ProfileScreen(),
+          // AppRoutes.MY_PETS: (ctx) => MyPetsScreen(),
+        }
+      ),
     );
   }
 }
--- perdi_meu_pet/lib/utils/app_routes.dart
+++ perdi_meu_pet/lib/utils/app_routes.dart
@@ -2,6 +2,7 @@ class AppRoutes {
   static const HOME = '/';
   static const POST_DETAIL = '/post-detail';
   static const PROFILE = '/profile';
+  static const MY_PETS = '/my-pets';
   // static const ADD_POST = '/add-post';
   // static const FAVORITES = '/favorites';
   // static const SETTINGS = '/settings'; // TODO: Implement settings page
