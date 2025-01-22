2742ecaeb9ed4bbe64be2875d671119a95202b5d
Gabriel Vinicius <gabrielviniciuslcm@gmail.com>
2025-01-22 06:46:36 -0300

    feat: teste com camera e imagem

--- perdi_meu_pet/lib/widgets/add_post_tab.dart
+++ perdi_meu_pet/lib/widgets/add_post_tab.dart
@@ -1,10 +1,14 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:perdi_meu_pet/domain/provider/post_provider.dart';
 import 'package:provider/provider.dart';
 import '../domain/model/pet.dart';
 import '../domain/model/post.dart';
 import '../domain/provider/pet_provider.dart';
 import '../domain/provider/user_provider.dart';
+
 class AddPostTab extends StatefulWidget {
   @override
   _AddPostTabState createState() => _AddPostTabState();
@@ -17,12 +21,10 @@ class _AddPostTabState extends State<AddPostTab> {
   String? _selectedPetId; // Track the selected pet's ID
   late PetProvider petProvider;
   Map<String, Pet> pets = {};
-  
   bool isLoading = true;
 
-
   @override
-  initState() {
+  void initState() {
     super.initState();
     petProvider = Provider.of<PetProvider>(context, listen: false);
     _fetchPets();
@@ -42,9 +44,24 @@ class _AddPostTabState extends State<AddPostTab> {
     }
   }
 
+  Future<void> _pickImage(ImageSource source) async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: source);
+
+    if (pickedFile != null) {
+      setState(() {
+        _imageUrl = pickedFile.path; // Usaremos o caminho local da imagem
+      });
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Nenhuma imagem foi selecionada.')),
+      );
+    }
+  }
+
   Future<void> _addPost() async {
     final userProvider = Provider.of<UserProvider>(context, listen: false);
-    final postProvier = Provider.of<PostProvider>(context, listen: false);
+    final postProvider = Provider.of<PostProvider>(context, listen: false);
     if (_selectedPetId != null &&
         _descricaoController.text.isNotEmpty &&
         _localizacaoController.text.isNotEmpty &&
@@ -52,15 +69,15 @@ class _AddPostTabState extends State<AddPostTab> {
       final newPost = Post(
         descricao: _descricaoController.text,
         localizacao: _localizacaoController.text,
-        imageUrl: _imageUrl!,
+        imageUrl: _imageUrl!, // Aqui você pode alterar para o URL após upload
         userId: userProvider.userId,
         petId: _selectedPetId!,
       );
 
       // Submit the post (e.g., save to a backend or update a local provider)
-      await postProvier.addPost(newPost);
+      await postProvider.addPost(newPost);
 
-      // For now, just clear the fields and show a success message
+      // Clear fields and show a success message
       _descricaoController.clear();
       _localizacaoController.clear();
       setState(() {
@@ -78,68 +95,69 @@ class _AddPostTabState extends State<AddPostTab> {
     }
   }
 
-  // Função para simular a escolha de uma imagem
-  void _pickImage() {
-    setState(() {
-      _imageUrl =
-          'https://blog.casadoprodutor.com.br/wp-content/uploads/2018/04/gatinho.jpg'; // Example image URL
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-
-
     return isLoading
-      ?Center(child: CircularProgressIndicator()) 
-      :Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            // Pet selection dropdown
-            DropdownButtonFormField<String>(
-              value: _selectedPetId,
-              onChanged: (value) {
-                setState(() {
-                  _selectedPetId = value;
-                });
-              },
-              items: pets.entries.map((entry) {
-                final pet = entry.value;
-                return DropdownMenuItem<String>(
-                  value: entry.key, // Pet ID
-                  child: Text(pet.name),
-                );
-              }).toList(),
-              decoration: InputDecoration(labelText: 'Selecione um Pet'),
+        ? Center(child: CircularProgressIndicator())
+        : Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                // Pet selection dropdown
+                DropdownButtonFormField<String>(
+                  value: _selectedPetId,
+                  onChanged: (value) {
+                    setState(() {
+                      _selectedPetId = value;
+                    });
+                  },
+                  items: pets.entries.map((entry) {
+                    final pet = entry.value;
+                    return DropdownMenuItem<String>(
+                      value: entry.key, // Pet ID
+                      child: Text(pet.name),
+                    );
+                  }).toList(),
+                  decoration: InputDecoration(labelText: 'Selecione um Pet'),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  controller: _descricaoController,
+                  decoration: InputDecoration(labelText: 'Descrição'),
+                  maxLines: 2,
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  controller: _localizacaoController,
+                  decoration: InputDecoration(labelText: 'Localização'),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton.icon(
+                      onPressed: () => _pickImage(ImageSource.gallery),
+                      icon: Icon(Icons.photo_library),
+                      label: Text('Galeria'),
+                    ),
+                    ElevatedButton.icon(
+                      onPressed: () => _pickImage(ImageSource.camera),
+                      icon: Icon(Icons.camera_alt),
+                      label: Text('Câmera'),
+                    ),
+                  ],
+                ),
+                if (_imageUrl != null) ...[
+                  SizedBox(height: 10),
+                  Image.file(File(_imageUrl!), height: 100, fit: BoxFit.cover),
+                ],
+                Spacer(),
+                ElevatedButton(
+                  onPressed: _addPost,
+                  child: Text('Publicar'),
+                ),
+              ],
             ),
-            SizedBox(height: 10),
-            TextField(
-              controller: _descricaoController,
-              decoration: InputDecoration(labelText: 'Descrição'),
-              maxLines: 2,
-            ),
-            SizedBox(height: 10),
-            TextField(
-              controller: _localizacaoController,
-              decoration: InputDecoration(labelText: 'Localização'),
-            ),
-            SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: _pickImage,
-              child: Text('Escolher Imagem'),
-            ),
-            if (_imageUrl != null) ...[
-              SizedBox(height: 10),
-              Image.network(_imageUrl!, height: 100, fit: BoxFit.cover),
-            ],
-            Spacer(),
-            ElevatedButton(
-              onPressed: _addPost,
-              child: Text('Publicar'),
-            ),
-          ],
-        ),
-      );
+          );
   }
 }
